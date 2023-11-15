0a40b859b395c5b53dc94df0f7eacdd36a8d5d75
RotemDevAcc <guyron2000@gmail.com>
2023-11-15 16:57:13 +0200

    Fixed an error while printing Receipt Model

--- Back/base/models.py
+++ Back/base/models.py
@@ -6,8 +6,6 @@ class Receipt(models.Model):
     products = models.TextField()
     price = models.FloatField(null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # Foreign key to User
-    def __str__(self):
-           return self.desc
 
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
